b50e08aa78499fb6a748a144f682e91b493399ba
Nerve-98 <rudtjq0331@gmail.com>
2020-12-25 22:01:44 +0900

    manager singleton

---
+++ Assets/Scripts/Managers/Manager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    static Manager s_instance;
+    public static Manager instance { get { init(); return s_instance; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    static void init()
+    {
+        if(s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Manager");
+            if(go == null)
+            {
+                go = new GameObject { name = "Manager" };
+                go.AddComponent<Manager>();
+            }
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Manager>();
+        }
+    }
+
+}
--- Assets/Scripts/Managers/Player.cs
+++ Assets/Scripts/Managers/Player.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloUnity : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Unity");
+        Manager mg = Manager.instance;
+
+
     }
 
     // Update is called once per frame
