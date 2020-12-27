23177a84215e81bdba2402c419b76724c9dc9f1c
Nerve-98 <rudtjq0331@gmail.com>
2020-12-28 01:42:26 +0900

    cammer tracking

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Define.CameraMode _mode = Define.CameraMode.QuarterView;
+
+    [SerializeField]
+    Vector3 _delta;
+
+    [SerializeField]
+    GameObject _player;
+
+
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = _player.transform.position + _delta;
+        transform.LookAt(_player.transform);
+    }
+}
